5ec42610870e38c122b85eef409f981515e88e88
Muhammad124hassan <bhaihassan036@gmail.com>
2024-02-21 03:29:39 -0800

    end level

--- script.js
+++ script.js
@@ -14,22 +14,23 @@ const options = {
 		
 		try {
 			const response = await fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city , options);
-			const result = await response.text();
+			const result = await response.json();
 			
-			// cloud_pct.innerHTML = response.cloud_pct
-			temp.innerHTML = response.temp
-			temp2.innerHTML = response.temp
-			feels_like.innerHTML = response.feels_like
-			humidity.innerHTML = response.humidity
-			humidity2.innerHTML = response.humidity
-			min_temp.innerHTML = response.min_temp
-	max_temp.innerHTML = response.max_temp
-	wind_speed.innerHTML = response.wind_speed
-	wind_speed2.innerHTML = response.wind_speed
-	wind_degrees.innerHTML = response.wind_degrees
-	sunrise.innerHTML = response.sunrise
-	sunset.innerHTML = response.sunset
+			// cloud_pct.innerHTML = result.cloud_pct
+			temp.innerHTML = result.temp
+			temp2.innerHTML = result.temp
+			feels_like.innerHTML = result.feels_like
+			humidity.innerHTML = result.humidity
+			humidity2.innerHTML = result.humidity
+			min_temp.innerHTML = result.min_temp
+	max_temp.innerHTML = result.max_temp
+	wind_speed.innerHTML = result.wind_speed
+	wind_speed2.innerHTML = result.wind_speed
+	wind_degrees.innerHTML = result.wind_degrees
+	sunrise.innerHTML = result.sunrise
+	sunset.innerHTML = result.sunset
 	
+	console.log(response);
 	console.log(result);
 	
 	
@@ -46,8 +47,8 @@ submit.addEventListener("click" , (e)=> {
 getWeather("Karachi")
 
 // try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
+// 	const result = await fetch(url, options);
+// 	const result = await result.text();
 // 	console.log(result);
 // } catch (error) {
 // 	console.error(error);
